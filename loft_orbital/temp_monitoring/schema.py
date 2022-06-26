ac0ed41dd767edc8d128b18f11aedd0e9f5c882b
Pierre Valentin <me@pierrevalentin.org>
2022-06-26 13:05:40 -0400

    gql schema

---
+++ loft_orbital/temp_monitoring/schema.py
@@ -0,0 +1,38 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from loft_orbital.temp_monitoring.models import Temperature
+
+
+class TemperatureType(DjangoObjectType):
+    class Meta:
+        model = Temperature
+        fields = ("timestamp", "value")
+
+
+class TemperatureStatisticsType(graphene.ObjectType):
+    min = graphene.Float()
+    max = graphene.Float()
+
+
+class Query(graphene.ObjectType):
+    current_temperature = graphene.Field(TemperatureType)
+    temperature_statistics = graphene.Field(TemperatureStatisticsType,
+                                            after=graphene.DateTime(), before=graphene.DateTime())
+
+    def resolve_current_temperature(root, info):
+        return Temperature.objects.latest('timestamp')
+
+    def resolve_temperature_statistics(root, info, after, before):
+        window = Temperature.objects.filter(
+            timestamp__gte=after, timestamp__lte=before).order_by('-value')
+        if not window:
+            return None
+        else:
+            return {
+                "min": window.first().value,
+                "max": window.last().value
+            }
+
+
+schema = graphene.Schema(query=Query)
